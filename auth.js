4f90f08b1cb5d1839ddc5a012f08dae778a7ed06
Saiinaruto <saipraveenabburi7@gmail.com>
2024-12-29 09:16:22 +0530

    2

--- auth.js
+++ auth.js
@@ -1,5 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
-import {getAuth,createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+
 // Fire base authentication
 const firebaseConfig = {
   apiKey: "AIzaSyAc-J55zmmzcP5oY0pYcBnp0CO_wxVXCM4",
@@ -12,27 +16,26 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-
 const signup = document.getElementById("signupBtn");
 
-signup.addEventListener("click" , (e)=>{
-    e.preventDefault();
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed up
-        const user = userCredential.user;
-        alert("Successfully Created")
-        window.location =  "./main.html"
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorMessage)
-        // ..
-      });
-})
+signup.addEventListener("click", (e) => {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      alert("Successfully Created");
+      window.location = "./main.html";
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      // ..
+    });
+});
 
 
